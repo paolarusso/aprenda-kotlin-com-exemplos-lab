71f22de5c98ee6954b454dde3ec0afcba8ee1585
Paola Russo <45703340+paolarusso@users.noreply.github.com>
2023-10-10 09:08:09 -0300

    docs: Modificação do arquivo desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,61 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val idade: Int, var email: String)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(
+    val nome: String,
+    val conteudos: List<ConteudoEducacional>,
+    val aluno: Usuario,
+    val nivel: List<Nivel>,
+    val inscritos: MutableList<Usuario> = mutableListOf(),
+) {
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val usuario1 = Usuario("Paola", 30, "paola@email.com")
+    val usuario2 = Usuario("André", 26,  "andre@email.com")
+    val usuario3 = Usuario("Venilton", 34, "venilton@email.com")
+    
+    val conteudo1 = ConteudoEducacional("Trabalhando em Equipes Ágeis", 180, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Tratamento de Exceções em Kotlin", 120, Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("Arquitetura Orientada a Eventos com Java, SpringBoot e Kafka", 180, Nivel.AVANCADO)
+    
+    val formacao1 = Formacao(
+        "Princípios de Desenvolvimento Ágil e Colaborativo",
+        listOf(conteudo1),
+        usuario1,
+        listOf(Nivel.BASICO)
+    )
+    
+    val formacao2 = Formacao(
+        "Desmistificando Kotlin para Programadores Java",
+        listOf(conteudo2, conteudo3),
+        usuario3,
+        listOf(Nivel.INTERMEDIARIO)
+    )
+    
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao2.matricular(usuario1)
+    formacao2.matricular(usuario3)
+
+    println("Alunos matriculados em Princípios de Desenvolvimento Ágil e Colaborativo:")
+    formacao1.inscritos.forEach { aluno ->
+        println("Nome: ${aluno.nome}, Idade: ${aluno.idade}, E-mail: ${aluno.email}")
+        
+    }
+    
+    println("Alunos matriculados em Desmistificando Kotlin para Programadores Java:")
+    formacao2.inscritos.forEach { aluno ->
+        println("Nome: ${aluno.nome}, Idade: ${aluno.idade}, E-mail: ${aluno.email}")
+        
+    }
+    
 }
